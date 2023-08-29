77090c4c3fac2d3d5f2078cf6640a67475fbb2e6
Nikitapil <nikitapiliavets@gmail.com>
2023-08-29 22:08:54 +0300

    add tests for todo store

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@pinia/nuxt'],
+  nitro: {
+    externals: {
+      inline: ["uuid"]
+    }
+  }
+})
---
+++ store/todo.ts
@@ -0,0 +1,60 @@
+import {defineStore} from "pinia";
+import { v4 as uuid } from 'uuid'
+
+export interface Todo {
+    id: string;
+    title: string;
+    done: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface TodoState {
+    items: Todo[];
+}
+
+export interface TodoAdd {
+    title: string;
+}
+
+export interface TodoUpdate {
+    title?: string;
+    done?: boolean;
+}
+
+const state = (): TodoState => ({
+    items: []
+});
+
+const getters = {
+    getById: (state: TodoState) => (id: string) => state.items.find(item => item.id === id),
+    getOrderedTodos: (state: TodoState) => [...state.items].sort((a: Todo, b: Todo) => a.createdAt.getTime() - b.createdAt.getTime())
+};
+
+const actions = {
+    add(partialTodo: TodoAdd) {
+        const todo: Todo = {
+            id: uuid(),
+            done: false,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            ...partialTodo,
+        }
+        this.items.push(todo);
+    },
+    remove(id: string) {
+        this.items = this.items.filter((item: Todo) => item.id !== id)
+    },
+    update(id: string, updatedTodo: TodoUpdate) {
+        const index = this.items.findIndex((item: Todo) => item.id === id)
+        if (index !== -1) {
+            this.items[index] = {...this.items[index], ...updatedTodo, updatedAt: new Date()}
+        }
+    }
+};
+
+export const useTodoStore = defineStore('todoStore', {
+    state,
+    getters,
+    actions
+})
