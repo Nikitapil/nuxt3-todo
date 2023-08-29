455fa314d0cb44ec8f0fffbfac0b478e97ac3f63
Nikitapil <nikitapiliavets@gmail.com>
2023-08-29 22:28:13 +0300

    add base template

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-full min-h-screen bg-gray-100">
+    <section class="text-center pt-7">
+      <h1 class="text-4xl text-gray-700">What are we doing today?</h1>
+    </section>
+    <section class="w-9/12 max-w-lg mx-auto flex items-center justify-center mt-7 bg-white shadow p-5">
+      <form @submit.prevent>
+        <input class="py-2 px-4 border border-blue-200 focus:outline-blue-300 rounded" type="text" placeholder="Add a todo...">
+        <button class="transition-all duration-300 border border-blue-200 py-2 px-4 ml-3 hover:bg-blue-200 rounded">Add Todo</button>
+      </form>
+    </section>
   </div>
 </template>
+<script setup lang="ts">
+</script>
