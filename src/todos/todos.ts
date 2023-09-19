b71b15621614a7858784ed6d45a01a4935aae736
Nikitapil <nikitapiliavets@gmail.com>
2023-09-19 23:07:48 +0300

    add getTodoList api method

--- src/todos/todos.ts
+++ src/todos/todos.ts
@@ -1,10 +1,18 @@
 import { Prisma } from '@prisma/client';
-import { CreateTodoOptions } from '~/src/todos/types';
+import { CreateTodoOptions, GetTodoListOptions } from '~/src/todos/types';
 import Joi from 'joi';
 
 const todoCreateOptionsSchema = Joi.object({
   title: Joi.string().max(255).required(),
   ownerid: Joi.string().required()
+});
+
+const getTodosListOptionsSchema = Joi.object({
+  ownerid: Joi.string().required(),
+  page: Joi.number().positive(),
+  limit: Joi.number().positive(),
+  isDone: Joi.boolean(),
+  search: Joi.string()
 });
 
 export class Todos {
@@ -29,5 +37,33 @@ export class Todos {
     } catch (e) {
       throw new Error('Error while creating todo');
     }
+  }
+
+  async getTodoList(getTodoListOptions: GetTodoListOptions) {
+    const params =
+      await getTodosListOptionsSchema.validateAsync(getTodoListOptions);
+    const { page = 1, limit = 10, isDone = null, search, ownerid } = params;
+    const offset = page * limit - limit;
+    const where: Prisma.TodoWhereInput = {
+      ownerid,
+      title: {
+        contains: search,
+        mode: 'insensitive'
+      }
+    };
+    if (isDone !== null) {
+      where.done = isDone;
+    }
+    const todos = await this.todoModel.findMany({
+      where,
+      skip: offset,
+      take: limit
+    });
+
+    return todos;
+  }
+
+  async editTodo() {
+
   }
 }
--- src/todos/types.ts
+++ src/todos/types.ts
@@ -2,3 +2,11 @@ export interface CreateTodoOptions {
   ownerid: string;
   title: string;
 }
+
+export interface GetTodoListOptions {
+  ownerid: string;
+  page?: number;
+  limit?: number;
+  isDone?: boolean;
+  search?: string;
+}
